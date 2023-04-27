db8ecbbc333eb91f1c15586a5937b16b6252d170
VoidSpren <juancho.chiquiza12@gmail.com>
2023-04-27 15:29:50 -0500

    añadido elemento RecetaAmpliada

---
+++ client/src/Components/RecetaAmpliada.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//uso <RecetaAmpliada receta="{receta}" ingredientes="{ingredientes}" usuario="{usuario}"><RecetaAmpliada/>
+const RecetaAmpliada = (props) => {
+    return (
+        <>
+            
+        </>
+    );
+};
