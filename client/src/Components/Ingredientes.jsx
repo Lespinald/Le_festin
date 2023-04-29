bcb6b546c466188b873630ca0a30c690c18927fe
Davidrg02 <davrodriguezgu@unal.edu.co>
2023-04-29 18:20:45 -0500

    Los ingredientes seleccionados se colocan en un arreglo y se muestran en la vista

--- client/src/Components/Ingredientes.jsx
+++ client/src/Components/Ingredientes.jsx
@@ -1,22 +1,25 @@
 import React, { useState } from "react";
 import "../Style/GUI_Ingredientes.css";
 import data from './Json/ingredientes.json'
-import { Modal } from "antd";
-import { useDispatch, useSelector } from "react-redux";
 
 const Ingredientes = () => {
   const [search_ingrediente, set_search_Ingrediente] = useState('');
   const [ingrediente, set_Ingrediente] = useState('');
+  const [ingredientes_elegidos, set_Ingredientes_Elegidos] = useState([]);
 
   console.log(search_ingrediente)
 
-  function elegirIngrediente(value) {
+  function agregar_ingredientes(value) {
     set_Ingrediente(value)
+    if (!ingredientes_elegidos.includes(value)){
+      set_Ingredientes_Elegidos(ingredientes_elegidos.concat(value))
+    }
   }
   console.log(ingrediente)
+  console.log(ingredientes_elegidos)
 
   return (
-    <div >
+    <div>
       <div className="componente_busqueda">
         <input 
           type="text" 
@@ -31,10 +34,31 @@ const Ingredientes = () => {
           className="lupa_icon" 
         />
       </div>
+      <h2 className="text_ingredientes_elegidos">
+        Ingredientes seleccionados:
+      </h2>
+      <div className="componente_elegidos">
+        <div className="grid-container_elegidos">
+          {ingredientes_elegidos.map((ingrediente, index) => (
+            <div 
+              key={index} 
+              className="grid-item_elegido"
+            >
+              {ingrediente}
+            </div>
+          ))}
+        </div>
+      </div>
       <div className="componente_Ingredientes">
-        <div className="grid-container">
+        <div className="grid-container_ingredientes">
           {data.ingredientes.map((ingrediente) => (
-            <button key={ingrediente.nombre} className="grid-item" onClick={() => elegirIngrediente(ingrediente.nombre)}>{ingrediente.nombre}</button>
+            <button 
+              key={ingrediente.nombre} 
+              className="grid-item_ingrediente" 
+              onClick={() => agregar_ingredientes(ingrediente.nombre)}
+            >
+              {ingrediente.nombre}
+            </button>
           ))}
         </div>
       </div>
