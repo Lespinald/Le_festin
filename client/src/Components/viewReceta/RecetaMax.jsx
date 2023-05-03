d4ab6057d6e6797d8ef0df2c3cc5286f4ac32906
VoidSpren <juancho.chiquiza12@gmail.com>
2023-05-03 16:53:06 -0500

    added back button

--- client/src/Components/viewReceta/RecetaMax.jsx
+++ client/src/Components/viewReceta/RecetaMax.jsx
@@ -24,9 +24,11 @@ const RecetaMax = (props) => {
       </div>
       <div className="PanelDerecho">
         <div className="TopOptions">
-          <svg style={{width: "2rem", height: "2rem"}} width="100" height="100" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10 27L26 9M9 9L26 27M18 2C26.8366 2 34 9.16344 34 18C34 26.8366 26.8366 34 18 34C9.16344 34 2 26.8366 2 18C2 9.16344 9.16344 2 18 2Z" stroke="black" strokeWidth="2.5"/>
-          </svg>
+          <a href="/receta">
+            <svg className="BackButton" width="100" height="100" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M10 27L26 9M9 9L26 27M18 2C26.8366 2 34 9.16344 34 18C34 26.8366 26.8366 34 18 34C9.16344 34 2 26.8366 2 18C2 9.16344 9.16344 2 18 2Z" stroke="black" strokeWidth="2.5"/>
+            </svg>
+          </a>
         </div>
         <div className="TextoReceta">
           <p>
