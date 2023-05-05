5ea671fb95c6e5d4a9e5965003dcb0ea2c3b7f7a
Juan Carreño <jcarrenoar@unal.edu.co>
2023-05-04 19:52:55 -0500

    ejemplo

--- client/src/Components/Receta.jsx
+++ client/src/Components/Receta.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import '../Style/styleReceta.css'
+import RecetaVentanPrincipal from './RecetaVentanPrincipal'
+import { useState } from 'react'
 
 const Receta = () => {
+  const [arreglo, setArreglo] = useState([1,2,3,4,5,6])
+  
   return (
     <div className='label'>
-      Receta
+      {arreglo.map((receta,index) => (
+        <RecetaVentanPrincipal key={index}/>
+      ))}
     </div>
   )
 }
---
+++ client/src/Components/RecetaVentanPrincipal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RecetaVentanPrincipal = () => {
+  return (
+    <div>
+      receta
+    </div>
+  )
+}
+
+export default RecetaVentanPrincipal
