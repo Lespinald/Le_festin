01b91cb19ef9c2166b8d29d36b7d9dcf861735c2
Davidrg02 <davrodriguezgu@unal.edu.co>
2023-05-04 22:04:04 -0500

    correccion en archivos de migracion

--- server/database/migrations/06-crear-tabla-creadas.js
+++ server/database/migrations/06-crear-tabla-creadas.js
@@ -8,7 +8,7 @@ create table if not exists Creadas(
     CONSTRAINT fk3_receta FOREIGN KEY (ID_receta) REFERENCES Receta (ID_receta)
     );
 `).then(() => {
-  console.log('Tabla de recetas creada');
+  console.log('Tabla de creadas creada');
 }).catch((err) => {
   console.error(err);
 })
