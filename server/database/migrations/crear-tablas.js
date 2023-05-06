6ddf84e8a698f6da8380b36beb17a47b2ee5e5b0
Davidrg02 <davrodriguezgu@unal.edu.co>
2023-05-06 17:32:57 -0500

    cambios en la base de datos, en las migraciones

--- server/database/migrations/crear-tablas.js
+++ server/database/migrations/crear-tablas.js
@@ -1,76 +1,73 @@
 const pool = require('../../config/database');
 
 pool.query(`
-create table if not exists Usuarios(
-  ID_usuario int NOT NULL PRIMARY KEY,
-  username VARCHAR(15) NOT NULL
+CREATE TABLE IF NOT EXISTS Usuarios (
+  ID_usuario VARCHAR(20) PRIMARY KEY,
+  username VARCHAR(15) NOT NULL UNIQUE
 );
-create unique index ux_usuarios_username on Usuarios(username);
-  
-create table if not exists Receta(
-  ID_receta int NOT NULL PRIMARY KEY,
+
+CREATE TABLE IF NOT EXISTS Receta (
+  ID_receta SERIAL PRIMARY KEY,
   nombre VARCHAR(40) NOT NULL,
   descripcion VARCHAR(100) NOT NULL,
   procedimiento TEXT NOT NULL,
   imagen TEXT
 );
 
-create table if not exists Ingrediente(
-  ID_ingrediente int NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Ingrediente (
+  ID_ingrediente SERIAL PRIMARY KEY,
   nombre VARCHAR(40) NOT NULL,
   imagen TEXT
-  );
+);
 
-create table if not exists IngredienteAsociado(
-  ID_ingrediente int NOT NULL,
-  ID_receta int NOT NULL,
+CREATE TABLE IF NOT EXISTS IngredienteAsociado (
+  ID_ingrediente SERIAL NOT NULL,
+  ID_receta SERIAL NOT NULL,
   CONSTRAINT fk_ingrediente FOREIGN KEY (ID_ingrediente) REFERENCES Ingrediente (ID_ingrediente),
   CONSTRAINT fk_receta FOREIGN KEY (ID_receta) REFERENCES Receta (ID_receta)
-  );
+);
 
-create table if not exists Favorito(
-  ID_ingrediente int NOT NULL,
-  ID_receta int NOT NULL,
+CREATE TABLE IF NOT EXISTS Favorito (
+  ID_ingrediente SERIAL NOT NULL,
+  ID_receta SERIAL NOT NULL,
   CONSTRAINT fk1_ingrediente FOREIGN KEY (ID_ingrediente) REFERENCES Ingrediente (ID_ingrediente),
   CONSTRAINT fk2_receta FOREIGN KEY (ID_receta) REFERENCES Receta (ID_receta)
-  );
+);
 
-create table if not exists Creadas(
-  ID_usuario int NOT NULL,
-  ID_receta int NOT NULL,
+CREATE TABLE IF NOT EXISTS Creadas (
+  ID_usuario VARCHAR(20) NOT NULL,
+  ID_receta SERIAL NOT NULL,
   CONSTRAINT fk1_usuario FOREIGN KEY (ID_usuario) REFERENCES Usuarios (ID_usuario),
   CONSTRAINT fk3_receta FOREIGN KEY (ID_receta) REFERENCES Receta (ID_receta)
-  );
+);
 
-create table if not exists Comentario(
-  ID_comentario int NOT NULL PRIMARY KEY,
-  ID_usuario int NOT NULL,
-  username VARCHAR(15) NOT NULL,
-  ID_receta int NOT NULL,
+CREATE TABLE IF NOT EXISTS Comentario (
+  ID_comentario SERIAL PRIMARY KEY,
+  ID_usuario VARCHAR(20) NOT NULL,
+  ID_receta SERIAL NOT NULL,
   comentario VARCHAR(200),
   CONSTRAINT fk2_usuario FOREIGN KEY (ID_usuario) REFERENCES Usuarios (ID_usuario),
   CONSTRAINT fk4_receta FOREIGN KEY (ID_receta) REFERENCES Receta (ID_receta)
-  );
+);
 
-create table if not exists Review(
-  ID_review int NOT NULL PRIMARY KEY,
-  ID_usuario int NOT NULL,
-  username VARCHAR(15) NOT NULL,
-  ID_receta int NOT NULL,
+CREATE TABLE IF NOT EXISTS Review (
+  ID_review SERIAL PRIMARY KEY,
+  ID_usuario VARCHAR(20) NOT NULL,
+  ID_receta SERIAL NOT NULL,
   review VARCHAR(200),
   CONSTRAINT fk3_usuario FOREIGN KEY (ID_usuario) REFERENCES Usuarios (ID_usuario),
   CONSTRAINT fk5_receta FOREIGN KEY (ID_receta) REFERENCES Receta (ID_receta)
-  );
+);
 
-create table if not exists PreguntasYSugerencias(
-  ID_pregunta int NOT NULL PRIMARY KEY,
-  ID_usuario int NOT NULL,
-  username VARCHAR(15) NOT NULL,
+CREATE TABLE IF NOT EXISTS PreguntasYSugerencias (
+  ID_pregunta SERIAL PRIMARY KEY,
+  ID_usuario VARCHAR(20) NOT NULL,
   textoPregunta VARCHAR(200),
   tipo VARCHAR(1),
   textoRespuesta VARCHAR(200),
   CONSTRAINT fk4_usuario FOREIGN KEY (ID_usuario) REFERENCES Usuarios (ID_usuario)
-  );
+);
+
 `).then(() => {
   console.log('Tabla de usuarios creada');
 }).catch((err) => {
