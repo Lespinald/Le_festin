263981bfe8d4e2344519842e85fd6bd002afef9e
Juan Carreño <jcarrenoar@unal.edu.co>
2023-05-06 09:32:01 -0500

    agregar datos a preguntas y segurencias

--- server/DatosProvisionales.sql
+++ server/DatosProvisionales.sql
@@ -1,7 +1,7 @@
 --Comando y formato para ingresar datos en la tabla Receta
 INSERT INTO Receta(ID_receta,nombre,descripcion,procedimiento,imagen) values
-(1,'Huevo frito','Delicioso plato práctico y fácil de elaborar, perfecto para desayunos','1.Colocar cerca de 30g de mantequilla en el sartén2.Romper el huevo y dejarlo caer sobre la mantequilla3.Dejar que se cocine el huevo cerca de 1 a 2 minutos y retirar','URL'),
-(2,'Salchicha frita','Salchichas fritas con mantequilla ideales para una comida rápida','1.Colocar cerca de 30g de mantequilla en el sartén2.Destapar la salcicha y dejarla caer sobre el sartén, bien sea picada o entera3.Dejar cocinar por cerca de 2 minutos y luego retirar','URL')
+(1,'Huevo frito','Delicioso plato prï¿½ctico y fï¿½cil de elaborar, perfecto para desayunos','1.Colocar cerca de 30g de mantequilla en el sartï¿½n2.Romper el huevo y dejarlo caer sobre la mantequilla3.Dejar que se cocine el huevo cerca de 1 a 2 minutos y retirar','URL'),
+(2,'Salchicha frita','Salchichas fritas con mantequilla ideales para una comida rï¿½pida','1.Colocar cerca de 30g de mantequilla en el sartï¿½n2.Destapar la salcicha y dejarla caer sobre el sartï¿½n, bien sea picada o entera3.Dejar cocinar por cerca de 2 minutos y luego retirar','URL')
 
 --Comando y formato para ingresar datos en la tabla Ingredientes
 INSERT INTO Ingrediente(ID_ingrediente,nombre,imagen) values
@@ -15,3 +15,10 @@ INSERT INTO IngredienteAsociado(ID_ingrediente,ID_receta) values
 (2,1),
 (2,2),
 (3,2)
+
+
+--Comando y formato para ingresar datos en la tabla IngredienteAsociado
+INSERT INTO preguntasysugerencias(id_pregunta,id_usuario,username,textopregunta,tipo,textorespuesta) values
+(1,1,'first usuario','Â¿CÃ³mo se realiza la busqueda de ingredientes?','P','Escriba el ingrediente a buscar'),
+(1,2,'second usuario','Deberian agregar mas recetas','S',''),
+(1,1,'first usuario','Pueden aÃ±adir strudel','P','No es pregunta')
