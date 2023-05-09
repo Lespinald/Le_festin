eb3099a5e9bf6da893a106f386160ef480fae87d
Davidrg02 <davrodriguezgu@unal.edu.co>
2023-05-09 16:23:27 -0500

    busqueda por ingredientes, nuevo metodo creado y ruta

--- client/src/Components/Receta.jsx
+++ client/src/Components/Receta.jsx
@@ -2,23 +2,29 @@ import React, { useEffect, useState } from "react";
 import '../Style/styleReceta.css'
 import RecetaVentanPrincipal from './RecetaVentanPrincipal'
 import data from './Json/receta.json'
-import VistaMinimaReceta from "./vistaMinimaReceta";
+import VistaMinimaReceta from "./VistaMinimaReceta";
 
 
 const Receta = (props) => {
   const [recetas, set_Recetas ] = useState([]);
-  // props.ingredientesSeleccionado
+  const ingredientesSeleccionados = props.ingredientesSeleccionados;
+  const objetoJson = JSON.stringify(ingredientesSeleccionados);
+
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/recetas')//ruta de la api
-        .then(response => response.json())
-        .then(datos => set_Recetas(datos)); // guardar todas las recetas
-  }, [props.ingredientesSeleccionado])
+    if(ingredientesSeleccionados.length === 0){
+      console.log("Arreglo vacio")
+      fetch('http://localhost:5000/api/recetas')//ruta de la api
+      .then(response => response.json())
+      .then(datos => set_Recetas(datos)); // guardar todas las recetas
+    }else{
+      console.log("Arreglo con ingredientes")
+      fetch(`http://localhost:5000/api/recetas/ingredientes/${objetoJson}`)//ruta de la api
+      .then(response => response.json())
+      .then(datos => set_Recetas(datos));
+    }
+  }, [ingredientesSeleccionados])
 
-  useEffect(() => {
-    // set_Recetas(data.recetas)
-  }, [recetas])
-  
   
   // props: ["ingrediente"]
 
