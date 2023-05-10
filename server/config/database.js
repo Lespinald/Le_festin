314b8d8834b602169faed18732d0fecdfaf9fabb
Davidrg02 <davrodriguezgu@unal.edu.co>
2023-05-09 23:21:26 -0500

    base de datos en el servidor, elephantSQL

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'LeFestin',
-  password: '2312002',
+  user: 'uydxpmdl',
+  host: 'silly.db.elephantsql.com',
+  database: 'uydxpmdl',
+  password: 'l5KaCHl6bWolt2n0sbyyrzjeIRwl40bG',
   port: 5432,
 });
 
