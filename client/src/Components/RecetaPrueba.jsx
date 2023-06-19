47f2bc83ee5d41db44863f248996120cedb921d0
Cosq <wpinilla@unal.edu.co>
2023-06-19 17:13:14 -0500

    agregado controlador getFavoritos

--- client/src/Components/RecetaPrueba.jsx
+++ client/src/Components/RecetaPrueba.jsx
@@ -2,18 +2,22 @@ import React, { useEffect, useState } from "react";
 import '../Style/styleReceta.css'
 import RecetaVentanPrincipal from './RecetaVentanPrincipal'
 import VistaMinimaReceta from "./VistaMinimaReceta";
+import {getFavoritos} from "../../../server/controllers/favoritosController";
 
 
 const RecetaPrueba = (props) => {
+  //const id_usuario = 'PuTZH5itSedMiZXThSPIztSztTj1';
   const [recetas, set_Recetas ] = useState([]);
   const ingredientesSeleccionados = props.ingredientesSeleccionados;
   const titulo = props.Titulo;
   const objetoJson = JSON.stringify(ingredientesSeleccionados);
+  
 
 
   useEffect(() => {
     if(ingredientesSeleccionados.length === 0){
       console.log("Arreglo vacio")
+      //console.log(getFavoritos(id_usuario));
       fetch('https://lefestin.onrender.com/api/recetas')//ruta de la api
       .then(response => response.json())
       .then(datos => set_Recetas(datos)); // guardar todas las recetas
--- server/controllers/favoritosController.js
+++ server/controllers/favoritosController.js
@@ -15,7 +15,21 @@ const postFavoritos = async(req, res) =>{
     }
 }
 
+const getFavoritos = async(req, res) =>{
+    try{
+        const id_usuario = req.id_usuario;
+        const response = await pool.query('SELECT id_receta FROM favorito WHERE id_usuario=$1',id_usuario);
+        res.json(response.rows);
+    }catch{
+        console.error(error);
+        res.status(500).send('Error al obtener favoritos');
+    }
+
+}
+
 
 module.exports = {// se exportan los métodos
-    postFavoritos
+    postFavoritos,
+    getFavoritos
+
 }
