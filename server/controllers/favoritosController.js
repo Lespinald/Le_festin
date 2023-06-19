6fe7f251a273ac898f536a8ca2c0968fafc01f60
Juan Carreño <jcarrenoar@unal.edu.co>
2023-06-19 02:29:31 -0500

    Arreglo query

--- server/controllers/favoritosController.js
+++ server/controllers/favoritosController.js
@@ -27,7 +27,7 @@ const deleteFavoritos = async(req, res) =>{
 }
 
 const getVerificarFavoritos = async(req, res) =>{
-    const{ id_usuario, id_receta} = req.body;
+    const{ id_usuario, id_receta} = req.params;
     console.log("antes del query")
     const response = await pool.query(`SELECT COUNT(*) AS existe_receta FROM favorito WHERE id_receta = '${id_receta}' AND id_usuario = '${id_usuario}'`);
     console.log("🚀 ~ file: favoritosController.js:34 ~ verificarFavoritos ~ response.rows:", response)
