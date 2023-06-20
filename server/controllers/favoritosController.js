25dbcf364d13aa9d1b8d05133e0ff0b022dc8ec4
Juan Carreño <jcarrenoar@unal.edu.co>
2023-06-19 23:25:15 -0500

    arreglo merge

--- server/controllers/favoritosController.js
+++ server/controllers/favoritosController.js
@@ -26,12 +26,14 @@ const deleteFavoritos = async(req, res) =>{
     }
 }
 
-const verificarFavoritos = async(req, res) =>{
-    const{ id_usuario, id_receta} = req.body;
-    
-    const response = await pool.query(`SELECT COUNT(*) AS existe_receta FROM favorito WHERE id_receta = '${id_receta}' AND id_usuario = '${id_usuario}';`);
-    console.log("🚀 ~ file: favoritosController.js:34 ~ verificarFavoritos ~ response.rows:", response.rows[0].existe_receta)
-    res.send(response.rows);
+const getVerificarFavoritos = async(req, res) =>{
+    const id_usuario = req.params.id_usuario;
+    const id_receta = req.params.id_receta;
+    console.log("antes del query")
+    const response = await pool.query(`SELECT COUNT(*) AS existe_receta FROM favorito WHERE id_receta = '${id_receta}' AND id_usuario = '${id_usuario}'`);
+    console.log("🚀 ~ file: favoritosController.js:34 ~ verificarFavoritos ~ response.rows:", response)
+    const respuesta = response.rows[0].existe_receta > 0;
+    res.send(respuesta);
 }
 
 const favoritosByIngredientes = async (req, res) => {
@@ -56,12 +58,11 @@ const favoritosByIngredientes = async (req, res) => {
     const response = await pool.query(query, values);
     res.json(response.rows);
   };
-  
-  
+
 
 module.exports = {// se exportan los métodos
     postFavoritos,
     deleteFavoritos,
-    verificarFavoritos,
+    getVerificarFavoritos,
     favoritosByIngredientes
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -9,7 +9,7 @@ const { getIngredienteAsociado, getIngredientesIdByRecetaID } = require('../cont
 const { getPreguntasSugerencias } = require('../controllers/preguntasySugerenciasController');
 const { getComentarios, postComentario, getComentariosbyReceta } = require ('../controllers/comentariosController');
 const { getPromedioReviewByRecetaID, postReview, getReviewsAll, verificarReview, getReviewByUserRecetaID } = require('../controllers/reviewController');
-const { postFavoritos, verificarFavoritos, deleteFavoritos,favoritosByIngredientes } = require ('../controllers/favoritosController')
+const { postFavoritos, getVerificarFavoritos, deleteFavoritos,favoritosByIngredientes } = require ('../controllers/favoritosController')
 
 //se definen los objetos de enrutamiento-----------------------------
 const usuariosRouter = express.Router();
@@ -68,7 +68,7 @@ reviewsRouter.get('/usuarioRecetaID/:id_usuario/:id_receta', getReviewByUserRece
 router.use('/api/favoritos', favoritosRouter);
 favoritosRouter.post('/crear/:id_usuario/:id_receta', postFavoritos);
 favoritosRouter.delete('/borrar/:id_usuario/:id_receta', deleteFavoritos);
-reviewsRouter.get('/verificar/:id_usuario/:id_receta', verificarFavoritos);
+favoritosRouter.get('/verificar/:id_usuario/:id_receta', getVerificarFavoritos);
 favoritosRouter.get('/favoritosByIngredientes/:ingredientes/:id_usuario', favoritosByIngredientes);
 //se exportan las rutas(router)-----------------------------------
 module.exports = router;
